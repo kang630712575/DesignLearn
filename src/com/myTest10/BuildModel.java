13da06e704be545df1492e3603d3021d30d8ab7a
kang630712575 <630712575@qq.com>
2020-07-31 21:47:48 +0800

    2020/07/31  适配器模式

--- src/com/myTest10/BuildModel.java
+++ src/com/myTest10/BuildModel.java
@@ -15,5 +15,139 @@ package com.myTest10;
  * 4、Director(指挥者)：构建一个使用builder接口的对象。他主要是用于创建一个复杂的对象，它主要有两个作用：①隔离了客户与对象的生产过程
  *                     ②负责控制产品对象的生产过程
  */
+
+/**
+ * 建造者模式在jdk的应用和源码分析
+ * 1、Appendable接口定义了多个append方法(抽象方法)，即Appendable为抽象建造者，定义了抽象方法
+ * 2、AbstractStringBuilder实现了Appendable接口方法，这里的AbstractStringBuilder已经是建造者了，只是不能实例化
+ * 3、StringBuilder即充当了指挥者角色，同时充当了具体的建造者，建造方法的实现是由AbstractStringBuilder完成，而StringBuilder
+ *    继承了AbstractStringBuilder
+ */
 public class BuildModel {
+
+    public static void main(String[] args) {
+        // 盖普通房子
+        CommonHouse commonHouse = new CommonHouse();
+        // 准备创建房子的指挥者
+        HouseDirector houseDirector = new HouseDirector(commonHouse);
+
+        // 完成房子
+        House house = houseDirector.constructorHouse();
+
+        System.out.println("输出流程");
+    }
 }
+
+class House{
+    private String baise;
+    private String wall;
+    private String roofed;
+
+    public String getBaise() {
+        return baise;
+    }
+
+    public void setBaise(String baise) {
+        this.baise = baise;
+    }
+
+    public String getWall() {
+        return wall;
+    }
+
+    public void setWall(String wall) {
+        this.wall = wall;
+    }
+
+    public String getRoofed() {
+        return roofed;
+    }
+
+    public void setRoofed(String roofed) {
+        this.roofed = roofed;
+    }
+}
+// 抽象建造者
+abstract class HouseBuilder{
+    protected House house = new House();
+
+    // 将建造流程写好
+    public abstract void buildBasic();
+    public abstract void buildWalls();
+    public abstract void roofed();
+
+    // 建造好后将产品返回
+    public House buildHouse(){
+        return house;
+    }
+}
+
+class CommonHouse extends HouseBuilder{
+
+    @Override
+    public void buildBasic() {
+        System.out.println("普通房子地基5米");
+    }
+
+    @Override
+    public void buildWalls() {
+        System.out.println("普通房子刷墙2平米");
+    }
+
+    @Override
+    public void roofed() {
+        System.out.println("普通房子房顶5米");
+    }
+}
+
+class HighBuilding extends HouseBuilder{
+
+    @Override
+    public void buildBasic() {
+        System.out.println("高楼地基10米");
+    }
+
+    @Override
+    public void buildWalls() {
+        System.out.println("高楼墙20cm");
+    }
+
+    @Override
+    public void roofed() {
+        System.out.println("高楼屋顶5米");
+    }
+}
+
+// 指挥者  动态指定制作流程，返回产品
+class HouseDirector{
+    HouseBuilder houseBuilder = null;
+
+    // 构造器传入houseBuilder
+    public HouseDirector(HouseBuilder houseBuilder) {
+        this.houseBuilder = houseBuilder;
+    }
+
+    // 通过setter 传入houseBuilder
+    public void setHouseBuilder(HouseBuilder houseBuilder) {
+        this.houseBuilder = houseBuilder;
+    }
+
+    // 如何处理建造房子的流程，交给指挥者
+    public House constructorHouse(){
+        houseBuilder.buildBasic();
+        houseBuilder.buildWalls();
+        houseBuilder.roofed();
+        return houseBuilder.buildHouse();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/com/myTest11/AdapterClass.java
@@ -0,0 +1,52 @@
+package com.myTest11;
+
+/**
+ * 适配器模式介绍
+ * 1、适配器模式(Adapter Pattern)将某个类的接口转换成客户端期望的另一个接口表示，主要目的是兼容性，让原本因接口不匹配不能一起工作
+ *    的两个类可以协同工作。其别名为包装器(Wrapper)
+ * 2、适配器模式数据结构性模式
+ * 3、主要分为三类：类适配器模式、对象适配器模式、接口适配器模式
+ */
+public class AdapterClass {
+
+}
+
+class Voltage220V{
+    public int output220V(){
+        int src = 220;
+        System.out.println("电压" + src + "V");
+        return src;
+    }
+}
+
+interface Voltage5V{
+    public int output5V();
+}
+
+
+
+class VoltageAdapter extends Voltage220V implements Voltage5V{
+
+    @Override
+    public int output5V() {
+        // 获取到220V电压
+        int srcV = output220V();
+        int dstV = srcV / 44;
+        return dstV;
+    }
+}
+
+class Phone{
+    //
+}
+
+
+
+
+
+
+
+
+
+
+
